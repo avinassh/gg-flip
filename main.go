2e6db981a920aaaf8468c433cd781da156cbf4d5
avi <hi@avi.im>
2018-01-20 12:34:38 +0530

    initial import

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+
+	funcMap := template.FuncMap{
+		"flip": func(i int) string {
+			if i == 0 {
+				return "-0"
+			}
+			return fmt.Sprintf("%d", ^i+1)
+		},
+	}
+	f, err := os.Create("lib.js")
+	t := template.Must(template.New("template.txt").Funcs(funcMap).ParseFiles("template.txt"))
+	err = t.Execute(f, make([]struct{}, 9007199254740991))
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
